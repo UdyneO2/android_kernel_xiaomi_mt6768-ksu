931975ac5c21bfe1290a6a79858cbff28c8d1b85
UdyneO2 <dynemuhamad@gmail.com>
2026-03-19 03:02:37 +0000

    cpufreq: Reflex CPUFreq Governor v0.2.2 Original patch: https://github.com/firelzrd/reflex/blob/main/patches/0001-Reflex-CPUFreq-Governor-v0.2.2.patch

--- include/linux/sched/cpufreq.h
+++ include/linux/sched/cpufreq.h
@@ -21,6 +21,18 @@ void cpufreq_add_update_util_hook(int cpu, struct update_util_data *data,
                        void (*func)(struct update_util_data *data, u64 time,
 				    unsigned int flags));
 void cpufreq_remove_update_util_hook(int cpu);
+
+/* Exported helpers for external cpufreq governors (e.g. loadable modules) */
+unsigned long cpufreq_get_capacity_ref_freq(struct cpufreq_policy *policy);
+unsigned long sugov_effective_cpu_perf(int cpu, unsigned long actual,
+				       unsigned long min, unsigned long max);
+void cpufreq_get_effective_util(int cpu, unsigned long boost,
+				unsigned long *out_util,
+				unsigned long *out_bw_min);
+bool cpufreq_cpu_dl_bw_exceeded(int cpu, unsigned long bw_min);
+bool cpufreq_cpu_uclamp_capped(int cpu);
+bool cpufreq_scx_switched_all(void);
+
 #endif /* CONFIG_CPU_FREQ */
 
 #endif /* _LINUX_SCHED_CPUFREQ_H */
