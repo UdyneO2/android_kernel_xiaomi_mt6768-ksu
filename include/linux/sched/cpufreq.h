123a9264a0ee080f49a32b16927e41a31b07fd2d
UdyneO2 <dynemuhamad@gmail.com>
2026-03-19 03:29:56 +0000

    beta

--- include/linux/sched/cpufreq.h
+++ include/linux/sched/cpufreq.h
@@ -11,7 +11,7 @@
 #define SCHED_CPUFREQ_RT	(1U << 0)
 #define SCHED_CPUFREQ_DL	(1U << 1)
 #define SCHED_CPUFREQ_IOWAIT	(1U << 2)
-
+struct cpufreq_policy;
 #ifdef CONFIG_CPU_FREQ
 struct update_util_data {
        void (*func)(struct update_util_data *data, u64 time, unsigned int flags);
